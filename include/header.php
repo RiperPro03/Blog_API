fa1b73879b7f51277ee73bd002645d980a4f48fa
RiperPro03 <as.riperpro.pf@gmail.com>
2023-03-30 21:45:30 +0200

    Suppression des fichier inutile

--- include/header.php
+++
@@ -1,6 +0,0 @@
-<?php
-    require_once './Model/ClientREST.php';
-    $client_monApi = new ClientREST('http://localhost/projet/TP-API/projet-r401/api/mon-api/');
-    $client_Auth = new ClientREST('http://localhost/projet/TP-API/projet-r401/api/auth/');
-
-?>
